a6df9ce10551499f71d66b7f0bb59ebdd0187d42
Bensas <juanbensa@gmail.com>
2023-12-25 12:16:03 -0300

    Added xbee serial port listener in a separate thread

--- communitation-module/src/main.rs
+++ communitation-module/src/main.rs
@@ -2,8 +2,20 @@
 mod controller;
 mod xbee;
 
+use std::sync::mpsc;
+
+use xbee::xbee::{listen_for_messages, LCMessage};
+
+struct AppState<'a> {
+    xbee_sender: mpsc::Sender<LCMessage<'a>>,
+}
+
 #[launch]
 fn rocket() -> _ {
+    let (xbee_sender, xbee_receiver) = mpsc::channel();
+    std::thread::spawn(|| listen_for_messages("/dev/ttys004", 0, xbee_receiver));
+
     rocket::build()
+        .manage(AppState { xbee_sender })
         .mount("/", routes![controller::controller::command_lc, controller::controller::system_status])
 }
--- communitation-module/src/xbee/xbee.rs
+++ communitation-module/src/xbee/xbee.rs
@@ -1,5 +1,41 @@
+use std::{time::Duration, io, sync::mpsc};
+
+#[derive(Debug)]
+pub struct LCMessage<'r> {
+    raw_content: &'r str,
+}
+
 
 
 pub fn send_command(raw_content: &str) {
   println!("Sending xbee message: {:?}", raw_content)
+}
+
+pub fn listen_for_messages(serial_port_name: &str, baud_rate: u32, rx: mpsc::Receiver<LCMessage>) {
+  // loop {}
+
+    let port = serialport::new(serial_port_name, baud_rate)
+        .timeout(Duration::from_millis(10))
+        .open();
+
+    match port {
+        Ok(mut port) => {
+            let mut serial_buf: Vec<u8> = vec![0; 1000];
+            println!("Receiving data on {} at {} baud:", &serial_port_name, &baud_rate);
+            loop {
+                match port.read(serial_buf.as_mut_slice()) {
+                    Ok(t) => {
+                      let bufferMessage: String = serial_buf[..t].into_iter().map(|&u|u as char).collect();
+                      println!("{:?}", bufferMessage)
+                    },
+                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+                    Err(e) => eprintln!("{:?}", e),
+                }
+            }
+        }
+        Err(e) => {
+            println!("Failed to open \"{}\". Error: {}", serial_port_name, e);
+            // ::std::process::exit(1);
+        }
+    }
 }
