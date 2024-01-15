f7379d2a9e8cf579fdbc1ecd8f128b27a3026e5c
Bensas <juanbensa@gmail.com>
2024-01-14 23:30:31 -0300

    Cleaned up App.tsx html

--- kranz-frontend/src/App.tsx
+++ kranz-frontend/src/App.tsx
@@ -7,28 +7,10 @@ import { SystemStatusProvider } from './providers/SystemStatusProvider'
 import SystemVisualizer from './components/SystemVisualizer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <SystemStatusProvider>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
+        <h1>Kranz Mission Control 🚀</h1>
         <p className="read-the-docs">
           Click on the Vite and React logos to learn more
         </p>
